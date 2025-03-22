eca0233f2559220f8451379ca4210df63d1eade5
6ixGod6 <jeromelar85@gmail.com>
2025-03-22 21:10:59 +0100

    Update User.java

--- app/src/main/java/com/example/contacts/models/User.java
+++ app/src/main/java/com/example/contacts/models/User.java
@@ -1,4 +1,23 @@
 package com.example.contacts.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+
+    @ColumnInfo(name = "fullname")
+    public String fullname;
+
+    @ColumnInfo(name = "email")
+    public String email;
+    @ColumnInfo(name = "phone")
+    public String phone;
+    @ColumnInfo(name = "isFavorite")
+    public boolean isFavorite;
+    @ColumnInfo(name = "photoPath")
+    public String photoPath;
 }
